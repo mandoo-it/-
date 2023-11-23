5b5b8ebd3710b2e25243e01970c70231f1f6b5da
윤규희 <ojinga0519@naver.com>
2023-11-23 21:07:57 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 20:42:40
+2023년 11월 4일 21:7:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -8,17 +8,16 @@ def solution(progresses, speeds):
         else :
             arr.append(int(haveTo/speed) + 1) 
 
-    cnt = 1
-    prev_day = arr[0]
-
-    for day in arr[1:]:
-        if prev_day >= day:  # 이전 작업보다 현재 작업이 오래 걸릴 경우
-            cnt += 1
-        else:  # 현재 작업이 더 빨리 완료될 경우
-            answer.append(cnt)
-            cnt = 1
-            prev_day = day
-
-    answer.append(cnt)  # 마지막 작업 배포
+    prev = arr[0]
+    count = 1
+    
+    for i in arr[1:]:
+        if i <= prev:
+            count += 1
+        else:
+            answer.append(count)
+            prev = i
+            count = 1
+    answer.append(count)
    
     return answer
